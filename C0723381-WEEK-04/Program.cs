c328f6c5f3e1e7254fe54dc05228d83df4694cba
Robindeep Singh <c0723381@mylambton.ca>
2019-01-30 11:20:14 -0500

    I added the countryside class

--- C0723381-WEEK-04/Program.cs
+++ C0723381-WEEK-04/Program.cs
@@ -18,8 +18,37 @@ namespace C0723381_WEEK_04
         // Node is an ADT
         // what KIND of DATA do we need in a nood?
         public Village nextVillage;
+        public Village previousVillage;
         public String VillageName;
         public bool isAstrildeHere = false;
         
+    }
+    class Countryside
+    {
+        Village Maple = new Village();
+        
+        Village Toronto = new Village();
+        
+        Village Ajax = new Village();
+        Village First;
+        Village Last;
+        
+        public void Launch()
+        {
+            Maple.VillageName = "Maple";
+            Maple.nextVillage = Toronto;
+            Maple.previousVillage = null;
+            Toronto.VillageName = "Toronto";
+            Toronto.nextVillage = Ajax;
+            Toronto.previousVillage = Ajax;
+            Ajax.VillageName = "Ajax";
+            Ajax.nextVillage = null;
+            Ajax.nextVillage = Toronto;
+
+
+        }
+
+
+
     }
 }
